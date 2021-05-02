b7dc96ea9690e2025a5e778787afdc1c0906bfc3
SaDevat <s.devat@gmail.com>
2021-05-02 20:15:42 +0530

    added the sidebar components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Sidebar from"./Sidebar";
 
 function App() {
   return <div className="app">
     <dv>Twitter clone</dv>
 
     {/* left static sidebar */}
+    <Sidebar/>
 
     {/* center feed */}
 
