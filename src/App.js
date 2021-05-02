8c856fcde4a2a6bd66b89df5e90733d9bbd8fc4b
SaDevat <s.devat@gmail.com>
2021-05-02 20:07:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return <div className="app">
+    <dv>Twitter clone</dv>
+
+    {/* left static sidebar */}
+
+    {/* center feed */}
+
+    {/* right static widget */}
+  </div>;
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        // we use bem convention
+        <div className="sidebar">
+            {/* twitter icon */}
+            {/* sidebar option component */}
+            {/* sidebar option component */}
+            {/* sidebar option component */}
+            {/* sidebar option component */}
+            {/* sidebar option component */}
+            {/* sidebar option component */}
+            {/* sidebar option component */}
+            {/* sidebar option component */}
+            {/* sidebar option component */}
+            {/* sidebar option component */}
+            {/* send out tweet button*/}
+        </div>
+    )
+}
+
+export default Sidebar
